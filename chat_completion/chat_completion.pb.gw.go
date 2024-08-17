63274b1aa29f11e66255a0450e1640ce733577cb
pkusunjy <767978963@qq.com>
2024-08-17 15:54:38 +0800

    update go version && add ielts_talk_report

--- chat_completion/chat_completion.pb.gw.go
+++ chat_completion/chat_completion.pb.gw.go
@@ -813,10 +813,37 @@ func local_request_ChatService_IeltsSpeakingExam_0(ctx context.Context, marshale
 
 }
 
+func request_ChatService_IeltsTalkReport_0(ctx context.Context, marshaler runtime.Marshaler, client ChatServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq ChatMessage
+	var metadata runtime.ServerMetadata
+
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.IeltsTalkReport(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_ChatService_IeltsTalkReport_0(ctx context.Context, marshaler runtime.Marshaler, server ChatServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq ChatMessage
+	var metadata runtime.ServerMetadata
+
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.IeltsTalkReport(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterChatServiceHandlerServer registers the http handlers for service ChatService to "mux".
 // UnaryRPC     :call ChatServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
 // Note that using this registration option will cause many gRPC library features to stop working. Consider using RegisterChatServiceHandlerFromEndpoint instead.
+// GRPC interceptors will not work for this type of registration. To use interceptors, you must use the "runtime.WithMiddlewares" option in the "runtime.NewServeMux" call.
 func RegisterChatServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux, server ChatServiceServer) error {
 
 	mux.Handle("POST", pattern_ChatService_Ask_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
@@ -1154,6 +1181,31 @@ func RegisterChatServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux
 
 	})
 
+	mux.Handle("POST", pattern_ChatService_IeltsTalkReport_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/chat_completion.ChatService/IeltsTalkReport", runtime.WithHTTPPathPattern("/chat_completion.ChatService/ielts_talk_report"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_ChatService_IeltsTalkReport_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_ChatService_IeltsTalkReport_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -1192,7 +1244,7 @@ func RegisterChatServiceHandler(ctx context.Context, mux *runtime.ServeMux, conn
 // to "mux". The handlers forward requests to the grpc endpoint over the given implementation of "ChatServiceClient".
 // Note: the gRPC framework executes interceptors within the gRPC handler. If the passed in "ChatServiceClient"
 // doesn't go through the normal gRPC flow (creating a gRPC client etc.) then it will be up to the passed in
-// "ChatServiceClient" to call the correct interceptors.
+// "ChatServiceClient" to call the correct interceptors. This client ignores the HTTP middlewares.
 func RegisterChatServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux, client ChatServiceClient) error {
 
 	mux.Handle("POST", pattern_ChatService_Ask_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
@@ -1965,6 +2017,28 @@ func RegisterChatServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux
 
 	})
 
+	mux.Handle("POST", pattern_ChatService_IeltsTalkReport_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/chat_completion.ChatService/IeltsTalkReport", runtime.WithHTTPPathPattern("/chat_completion.ChatService/ielts_talk_report"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_ChatService_IeltsTalkReport_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_ChatService_IeltsTalkReport_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -2038,6 +2112,8 @@ var (
 	pattern_ChatService_IeltsSpeakingExercise_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"chat_completion.ChatService", "ielts_speaking_exercise"}, ""))
 
 	pattern_ChatService_IeltsSpeakingExam_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"chat_completion.ChatService", "ielts_speaking_exam"}, ""))
+
+	pattern_ChatService_IeltsTalkReport_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"chat_completion.ChatService", "ielts_talk_report"}, ""))
 )
 
 var (
@@ -2110,4 +2186,6 @@ var (
 	forward_ChatService_IeltsSpeakingExercise_0 = runtime.ForwardResponseMessage
 
 	forward_ChatService_IeltsSpeakingExam_0 = runtime.ForwardResponseMessage
+
+	forward_ChatService_IeltsTalkReport_0 = runtime.ForwardResponseMessage
 )
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/pkusunjy/openai-server-proto
 
-go 1.22.2
+go 1.23.0
 
 require (
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.20.0
